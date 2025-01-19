01ac5d003f3a295f54741cc1dbcce9ee5bd8d98c
FarisET1 <faris@emergtechsolutions.com>
2025-01-19 19:48:17 +0500

    first commit

---
+++ components/ui/logo.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="inline-flex space-x-2" aria-label="Safify">
+      <img
+        src="/images/safify-logo.png"
+        alt="Safify Logo"
+        className="h-7 w-7"
+      />
+      <span className="hidden md:block text-lg text-gray-800">SAFIFY</span>
+
+    </Link>
+  );
+}
